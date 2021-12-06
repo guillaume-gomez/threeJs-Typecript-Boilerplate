12a7c9fea9de797a34d89eecb14673347030064b
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-12-06 11:17:45 +0100

    add animation

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,6 @@
-import './style.css'
-import * as THREE from 'three'
+import './style.css';
+import * as THREE from 'three';
+import gsap from 'gsap';
 
 // Scene
 const scene = new THREE.Scene();
@@ -9,8 +10,8 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00Afff });
 const mesh = new THREE.Mesh(geometry, material);
 
-
 scene.add(mesh);
+
 
 // Sizes
 const sizes = {
@@ -18,8 +19,8 @@ const sizes = {
     height: 600
 }
 // Axe Helper
-const axesHelper = new THREE.AxesHelper(2)
-scene.add(axesHelper)
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
@@ -30,5 +31,23 @@ scene.add(camera);
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('canvas.webgl')
 });
+
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+//renderer.render(scene, camera);
+
+/**
+ * Animate
+ */
+gsap.to(mesh.rotation, { duration: 10, delay: 1, x: 2 });
+
+
+function tick()
+{
+    // Render
+    renderer.render(scene, camera);
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick);
+}
+
+tick();
