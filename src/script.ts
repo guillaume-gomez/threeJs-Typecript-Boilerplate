7c5acbb6cae9d6360737cdb30f14f4d03da4f46d
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-12-06 10:32:00 +0100

    update cube color and add axis helper

--- src/script.ts
+++ src/script.ts
@@ -1,29 +1,37 @@
 import './style.css'
 import * as THREE from 'three'
 
+interface test {
+    manger: string;
+}
 // Scene
 const scene = new THREE.Scene();
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0f0f });
+const material = new THREE.MeshBasicMaterial({ color: 0x00Afff });
 const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh)
+
+
+scene.add(mesh);
 
 // Sizes
 const sizes = {
-    width: 1000,
-    height: 400
+    width: 800,
+    height: 600
 }
+// Axe Helper
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('canvas.webgl')
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
